674593cf029dd6300de229a7c62b97490f777534
YuriP123 <miguel.pasamonte2@gmail.com>
2025-04-04 19:32:33 -0700

    feat: added top artist call

--- src/main.py
+++ src/main.py
@@ -3,16 +3,39 @@ from dotenv import load_dotenv
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+import spotipy.util as util
 
 load_dotenv()
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 redirect_uri = os.getenv('REDIRECT_URI')
+username = os.getenv('SPOTIFY_USERNAME')
 
 scope = 'user-library-read'
-sp = Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri,scope=scope))
+artistScope = 'user-top-read'
 
-user = sp.current_user()
-print(f'User: {user["display_name"]}')
-print(f'ID: {user["id"]}')
-print(f'URI: {user["uri"]}')
+## Sample Class ##
+# class User:
+#     def __init__(self,name,artistArr ,songArr):
+#         self.username = name
+#         self.artists = artistArr
+#         self.songs = songArr
+
+def main():
+    sp = Spotify(auth_manager=SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri,scope=artistScope))
+
+    if sp:
+        user = sp.current_user()
+        top_artists = sp.current_user_top_artists(limit=50, time_range="medium_term")
+        count = 0
+
+        print(f'User: {user["display_name"]}')
+        print(f'ID: {user["id"]}')
+        print(f'URI: {user["uri"]}')
+        print("----- Top 50 Artists ----")
+        for i, artist in enumerate(top_artists["items"],1):
+            print(f"{i}: {artist["name"]}")
+
+main();
+
+
